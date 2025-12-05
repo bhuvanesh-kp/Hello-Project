34b493dfad5d4ed74a36e2060ba2c125c03cf90c
bhuvanesh-kp <bhuvaneshkpb@gmail.com>
2025-12-05 16:28:12 +0530

    c2

--- src/main/java/com/tarun/help/demo/Controller/HelperEndpoint.java
+++ src/main/java/com/tarun/help/demo/Controller/HelperEndpoint.java
@@ -1,9 +1,11 @@
 package com.tarun.help.demo.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class HelperEndpoint {
     @GetMapping("/hello")
     public String Service1(){
